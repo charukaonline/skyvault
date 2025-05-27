c9456d197e23ec756eab8394fae38ed61e8bdbed
charukaonline <nuwanthakonline@gmail.com>
2025-05-28 00:55:40 +0530

    db connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
