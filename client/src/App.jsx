92d5a66e5bde663879a3f42a0d4acd3ea51ddee2
charukaonline <nuwanthakonline@gmail.com>
2025-05-28 00:12:50 +0530

    #

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <Button variant="default">Click me</Button>
     </div>
   );
 }
