230be101fba51a497d92aa873ab2140ba47c4b1a
charukaonline <nuwanthakonline@gmail.com>
2025-06-03 18:58:01 +0530

    #

--- server/src/main/java/com/skyvault/server/ServerApplication.java
+++ server/src/main/java/com/skyvault/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.skyvault.server.server;
+package com.skyvault.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
