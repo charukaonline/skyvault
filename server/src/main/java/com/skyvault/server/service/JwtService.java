e2950c04ca9a6ceb8f7851d428da69ded2a3e48f
charukaonline <nuwanthakonline@gmail.com>
2025-06-04 17:18:48 +0530

    #

--- server/src/main/java/com/skyvault/server/service/JwtService.java
+++ server/src/main/java/com/skyvault/server/service/JwtService.java
@@ -8,7 +8,6 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
